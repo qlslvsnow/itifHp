390033c129cc20456865bcf29111e2c56df9b6f0
LvRJ22 <qlslvsnow@yeah.net>
2016-06-14 20:45:16 +0900

    fix issue update

--- js/customItif.js
+++ js/customItif.js
@@ -1,34 +1,29 @@
-      /*menu handler*/
-      $(function(){
-        function stripTrailingSlash(str) {
-          if(str.indexOf("/") >= 0) {
-            return str.substr(1, str.length - 6);
-          }
-          return str.substr(0, str.length - 5);
-        }
+/*menu handler*/
+$(function(){
+  function stripTrailingSlash(str) {
+    if(str.indexOf("/") >= 0) {
+      return str.substr(1, str.length - 6);
+    }
+    return str.substr(0, str.length - 5);
+  }
 
-        var url = window.location.pathname;  
-        var activePage = stripTrailingSlash(url);
+  var url = window.location.pathname;  
+  var activePage = stripTrailingSlash(url);
 
-        // var menuEnForjp = new Map();
-        // // setting the values
-        // menuEnForjp.set("company", "会社情報");
-        // menuEnForjp.set("business", "事業内容");
-        // menuEnForjp.set("recruit", "採用情報");
-        // menuEnForjp.set("contact", "お問い合せ");
+  $('.nav li a').each(function(){  
+    var currentPage = stripTrailingSlash($(this).attr('href'));
 
-        $('.nav li a').each(function(){  
-          var currentPage = stripTrailingSlash($(this).attr('href'));
-
-          if (activePage == currentPage) {
-            $(this).parent().addClass('active'); 
-          } 
-        });
-      });
+    if (activePage == currentPage) {
+      $(this).parent().addClass('active'); 
+    } 
+  });
+});
 
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
+        $('#navbar-hamburger').toggleClass('hidden');
+        $('#navbar-close').toggleClass('hidden');  
     }
 });
 
