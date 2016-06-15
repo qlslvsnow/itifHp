2784fc15c39ec35e129e9b03b0ff79d3628ad362
LvRJ22 <qlslvsnow@yeah.net>
2016-06-15 23:47:22 +0900

    update

--- js/customItif.js
+++ js/customItif.js
@@ -42,89 +42,110 @@ $(document).ready(function() {
                         min: 2,
                     },
                         notEmpty: {
-                        message: 'Please supply your first name'
+                        // message: 'Please supply your first name'
+                        message: 'ご担当者様氏名を入力してください'
                     }
                 }
             },
-             last_name: {
+            last_name: {
                 validators: {
                      stringLength: {
                         min: 2,
                     },
                     notEmpty: {
-                        message: 'Please supply your last name'
+                        // message: 'Please supply your last name'
+                        message: 'フリガナを入力してください'
                     }
                 }
             },
+            sex: {
+                validators: {
+                    // stringLength: {
+                    //     min: 2,
+                    // },
+                    notEmpty: {
+                        // message: 'Please supply your last name'
+                        message: '性別を選択してください'
+                    }
+                }
+            },
+
             email: {
                 validators: {
                     notEmpty: {
-                        message: 'Please supply your email address'
+                        // message: 'Please supply your email address'
+                        message: 'メールアドレスを入力してください'
                     },
                     emailAddress: {
-                        message: 'Please supply a valid email address'
+                        //message: 'Please supply a valid email address'
+                        message: 'メールアドレスの記述に誤りがあります'
                     }
                 }
             },
             phone: {
                 validators: {
                     notEmpty: {
-                        message: 'Please supply your phone number'
+                        // message: 'Please supply your phone number'
+                        message: '電話番号を入力してください'
                     },
                     phone: {
-                        country: 'US',
-                        message: 'Please supply a vaild phone number with area code'
+                        country: 'JP',
+                        // message: 'Please supply a vaild phone number with area code'
+                        message: '電話番号を入力してください'
                     }
                 }
             },
             address: {
                 validators: {
-                     stringLength: {
-                        min: 8,
-                    },
+                    // stringLength: {
+                    //     min: 8,
+                    // },
                     notEmpty: {
-                        message: 'Please supply your street address'
+                        message: '住所を入力してください'
+                        // message: 'Please supply your street address'
                     }
                 }
             },
-            city: {
-                validators: {
-                     stringLength: {
-                        min: 4,
-                    },
-                    notEmpty: {
-                        message: 'Please supply your city'
-                    }
-                }
-            },
-            state: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please select your state'
-                    }
-                }
-            },
-            zip: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please supply your zip code'
-                    },
-                    zipCode: {
-                        country: 'US',
-                        message: 'Please supply a vaild zip code'
-                    }
-                }
-            },
+            // city: {
+            //     validators: {
+            //          stringLength: {
+            //             min: 4,
+            //         },
+            //         notEmpty: {
+            //             message: 'Please supply your city'
+            //         }
+            //     }
+            // // },
+            // state: {
+            //     validators: {
+            //         notEmpty: {
+            //             message: 'Please select your state'
+            //         }
+            //     }
+            // },
+            // zip: {
+            //     validators: {
+            //         notEmpty: {
+            //             message: 'Please supply your zip code'
+            //         },
+            //         zipCode: {
+            //             country: 'US',
+            //             message: 'Please supply a vaild zip code'
+            //         }
+            //     }
+            // },
             comment: {
                 validators: {
-                      stringLength: {
-                        min: 10,
-                        max: 200,
-                        message:'Please enter at least 10 characters and no more than 200'
-                    },
-                    notEmpty: {
-                        message: 'Please supply a description of your project'
-                    }
+                      // stringLength: {
+                      //   min: 10,
+                      //   max: 200,
+                        // message:'Please enter at least 10 characters and no more than 200'
+                        // message: 'お問い合わせ詳細'
+                        // },
+                        notEmpty: {
+                            // message: 'Please supply a description of your project'
+                            message: 'お問い合わせ詳細を入力してください'
+                        }
                     }
                 }
             }
